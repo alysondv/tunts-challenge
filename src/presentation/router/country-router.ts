11cfd5bb8e9191bb82e6b40adb9d1391e9e981cf
alysondv <alysonhenrique26@gmail.com>
2022-08-25 11:37:09 -0300

    feat: add middleware

---
+++ src/presentation/router/country-router.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import { Request, Response } from 'express'
+import { GetAllCountriesUseCase } from "../../domain/use-cases/country/get-all-countries";
+
+
+export default function ContactsRouter(
+    GetAllCountriesUseCase: GetAllCountriesUseCase,
+) {
+    const router = express.Router()
+
+    router.get('/', async (req: Request, res: Response) => {
+        try {
+            const countries = await GetAllCountriesUseCase.execute()
+            res.send(countries)
+        } catch (err) {
+            res.status(500).send({ message: "Error fetching data" })
+        }
+    })
+}
